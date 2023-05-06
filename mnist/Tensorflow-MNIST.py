f1609cb8e8986cdbd3eabbf2f567623f7e764ae8
MsterAB <ducahmed@icloud.com>
2023-05-06 11:41:57 -0400

    Tensorflow_MNIST

--- mnist/Tensorflow-MNIST.py
+++ mnist/Tensorflow-MNIST.py
@@ -4,7 +4,6 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train/255, x_test/255
 
-from keras.layers.core.activation import Activation
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28,28)),
     tf.keras.layers.Dense(128, activation='relu'),
